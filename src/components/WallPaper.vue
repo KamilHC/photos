1135ce902ab2cbd53f88f12d8fe811ddf7374d97
KamilHC <kamil_malaszewicz@o2.pl>
2020-08-30 11:09:55 +0200

    Dodanie src do repo

---
+++ src/components/WallPaper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wallpaperWrapper">
+  </div>
+</template>
+
+<script>
+  
+export default {
+  name: 'WallPaper',
+}
+  
+</script>
+
+<style lang="scss" scoped>
+
+  .wallpaperWrapper {
+    // background-image: url("../assets/piramid.jpg");
+    // width: 1500px;
+    // height: 1500px;
+  }
+
+</style>
